9615eb861077b2dca4c777faf9e6e4b2a49c57b5
OC2 <oren1@Mac-1.local>
2016-04-20 11:35:11 +0300

    First Upload
    wsds

---
+++ js/app.js
@@ -0,0 +1,135 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('pele', ['ionic'
+                           ,'ngCordova'
+                           ,'ngStorage'
+                           ,'tabSlideBox'
+                           ,'pele.controllers'
+                           ,'pele.factories'
+                           ,'pele.config'
+                           ,'pele.services'
+                           ,'fileLogger'
+                          ])
+
+.run(function($ionicPlatform , $state , $ionicLoading , $fileLogger , PelApi , appSettings  ) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+
+    //-----------------------------------------
+    //--   Registration for Push Notification
+    //-----------------------------------------
+    var notificationOpenedCallback = function(jsonData) {
+    };
+    if(window.plugins !== undefined) {
+      window.plugins.OneSignal.init("1d0135a7-da67-4953-b241-2385bfcedcd9", {googleProjectNumber: "655668363586"}, notificationOpenedCallback);
+
+      window.plugins.OneSignal.getIds(function(ids) {
+        //document.getElementById("OneSignalUserID").innerHTML = "UserID: " + ids.userId;
+        //document.getElementById("OneSignalPushToken").innerHTML = "PushToken: " + ids.pushToken;
+        console.log('getIds: ' + JSON.stringify(ids));
+      });
+    }
+
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+      //----------------------------------------
+      //--    Get Version from config.xml
+      //----------------------------------------
+      window.cordova.getAppVersion(function (version) {
+        config_app.APP_VERSION = version;
+      });
+      console.log("VERSION : " + config_app.APP_VERSION);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    //----------------------------------
+    //--    Create/Open Log File
+    //----------------------------------
+    $fileLogger.setStorageFilename(config_app.LOG_FILE_NAME);
+    //----------------------------------
+    //--   Delete Old Log File data
+    //----------------------------------
+    $fileLogger.deleteLogfile();
+    //----------------------------------
+    //--  Write open row to log file
+    //----------------------------------
+    PelApi.writeToLog(config_app.LOG_FILE_INFO_TYPE ,'=============== Start ==============');
+
+    //----------------------------------
+    //--    Go To Application List
+    //----------------------------------
+    $state.go("app.p1_appsLists");
+
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  //---- P1 ----//
+  .state('app.p1_appsLists', {
+      url: '/p1_appsLists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/p1_appsLists.html',
+          controller: 'P1_appsListCtrl'
+        }
+      }
+    })
+    //---- home ----//
+    .state('app.home', {
+      url: '/home/:showLoading',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/p1_appsLists.html',
+          controller: 'homeCtrl'
+        }
+      }
+    })
+    .state('app.settings', {
+        url: '/settings',
+        views:{
+          'menuContent': {
+            templateUrl: 'templates/settings/settingsList.html',
+            controller: 'SettingsListCtrl'
+          }
+        }
+    })
+    .state('app.settings.sendLog', {
+        url: '/sendLog',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/settings/sendLog.html',
+            controller: 'SendLogCtrl'
+          }
+        }
+  })
+  .state('app.appProfile', {
+      url: '/appProfile',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/settings/appProfile.html',
+          controller: 'AppProfileCtrl'
+        }
+      }
+    })
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home',{'showLoading':'Y'});
+});
---
+++ js/controllers.js
@@ -0,0 +1,399 @@
+angular.module('pele.controllers', ['ngStorage'])
+angular.module('pele.controllers', ['ngStorage'])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $cordovaNetwork , $rootScope , appSettings , $state ) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+    document.addEventListener("deviceready", function () {
+
+      config_app.network = $cordovaNetwork.getNetwork();
+      config_app.isOnline = $cordovaNetwork.isOnline();
+      $scope.$apply();
+
+      // listen for Online event
+      $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+        config_app.isOnline = true;
+        config_app.network = $cordovaNetwork.getNetwork();
+        $scope.$apply();
+      })
+
+      // listen for Offline event
+      $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+        config_app.isOnline = false;
+        config_app.network = $cordovaNetwork.getNetwork();
+
+        $scope.$apply();
+      })
+
+    }, false);
+
+    if(appSettings.enviroment === "PD"){
+      $scope.myClass = "envPD";
+    }
+    if(appSettings.enviroment === "QA"){
+      $scope.myClass = "envQA";
+      //$scope.myClass = "envPD";
+    }
+    if(appSettings.enviroment === "DV"){
+      $scope.myClass = "envDV";
+    }
+    //===============================================//
+    //== Forward to selected option from menu list ==//
+    //===============================================//
+    $scope.forwardTo = function(statePath){
+      $state.go(statePath);
+    }
+    //===============================================
+    //==             isShowLogOut
+    //===============================================
+    $scope.isShowLogOut = function(){
+      var deviceInformation = ionic.Platform.device();
+      var isWebView = ionic.Platform.isWebView();
+      var isIPad = ionic.Platform.isIPad();
+      var isIOS = ionic.Platform.isIOS();
+      var isAndroid = ionic.Platform.isAndroid();
+      var isWindowsPhone = ionic.Platform.isWindowsPhone();
+
+      if(isAndroid){
+      $scope.menu.isShowLogOut = true;
+      }
+      else{
+        $scope.menu.isShowLogOut = false;
+      }
+    }
+    //===============================================//
+    //==            Log Out                        ==//
+    //===============================================//
+    $scope.logout  = function() {
+      isAndroid = ionic.Platform.isAndroid();
+
+      if(isAndroid){
+        ionic.Platform.exitApp();
+      }
+    } ;
+    $scope.menu ={};
+    $scope.isShowLogOut();
+})
+//=====================================================================//
+//==                        homeCtrl                                 ==//
+//=====================================================================//
+.controller('homeCtrl' , function($scope , $http , $state , $ionicLoading , PelApi , $cordovaNetwork , $rootScope , $ionicPopup, $stateParams){
+    var showLoading = $stateParams.showLoading;
+    if("Y" === showLoading){
+      PelApi.showLoading();
+    }
+}) // homeCtrl
+//=====================================================================//
+//==                      Setings SendLog                            ==//
+//=====================================================================//
+.controller('SendLogCtrl' , function($scope){
+}) // SendLogCtrl
+
+//=====================================================================//
+//==                      Setings SendLog                            ==//
+//=====================================================================//
+
+
+
+//=====================================================================//
+//==                         PAGE_1                                  ==//
+//=====================================================================//
+.controller('P1_appsListCtrl', function($scope
+                                      , $http
+                                      , $state
+                                      , $ionicLoading
+                                      , PelApi
+                                      , $cordovaNetwork
+                                      , $rootScope
+                                      , $ionicPopup
+                                      , $ionicHistory
+                                      , $sessionStorage
+                                      , appSettings
+                                      ) {
+
+  $ionicHistory.clearHistory();
+  //=======================================================//
+  //== When        Who         Description               ==//
+  //== ----------  ----------  ------------------------- ==//
+  //== 27/12/2015  R.W.                                  ==//
+  //=======================================================//
+    $scope.pushBtnClass = function(event){
+
+      console.log("pushBtnClass : " + event);
+
+      if(event === true){
+        return "pele-item-on-release";
+      }else{
+        return "pele-item-on-touch";
+      }
+    }// pushBtnClass
+
+  $scope.onBtnAction = function(){
+    btnClass.activ = !btnClass.activ;
+  };
+  $scope.doRefresh = function(){
+    $scope.btn_class = {};
+    $scope.btn_class.on_release = true;
+
+    PelApi.showLoading();
+    var errorMsg = "";
+
+    $scope.isOnline = config_app.isOnline;
+    $scope.network = config_app.network;
+    if("wifi" === config_app.network){
+      $ionicLoading.hide();
+      $scope.$broadcast('scroll.refreshComplete');
+      $scope.feeds_categories = {};
+      PelApi.showPopup(config_app.wifiTitle , config_app.wifiSubTitle);
+    }
+    else{
+
+
+    var links = PelApi.getDocApproveServiceUrl("GetUserMenu");
+
+    try{
+    var reMenu = PelApi.getMenu(links);
+    }catch(e){
+      var isAndroid = ionic.Platform.isAndroid();
+
+      if(isAndroid){
+        $ionicLoading.hide();
+        $scope.$broadcast('scroll.refreshComplete');
+        window.location = "./index.html" ;
+      }
+    }
+
+
+    reMenu.then(
+      //--- SUCCESS ---//
+      function () {
+
+        reMenu.success(function (data, status, headers, config) {
+
+          PelApi.writeToLog(config_app.LOG_FILE_INFO_TYPE , JSON.stringify(data));
+
+          console.log("============ GetMenu SUCCESS =============");
+          console.log(JSON.stringify(data));
+          console.log("==========================================");
+
+          var pinCodeStatus = PelApi.GetPinCodeStatus(data, "getMenu");
+          if("Valid" === pinCodeStatus){
+
+            config_app.token    = data.token;
+            console.log("TOKEN : " + data.token);
+            config_app.user     = data.user;
+            config_app.userName = data.userName;
+            var strData = JSON.stringify(data);
+            strData = strData.replace(/\"\"/g,null);
+            strData = strData.replace(/"\"/g,"");
+            config_app.GetUserMenu = JSON.parse(strData);
+            $scope.feeds_categories = config_app.GetUserMenu;
+
+            //---------------------------------------------
+            //-- Send User Tag for push notifications
+            //---------------------------------------------
+            if(window.plugins !== undefined){
+
+              console.log("ENVIRONMENT : " + appSettings.enviroment);
+
+              window.plugins.OneSignal.sendTags({"User": data.userName , "Env": appSettings.enviroment});
+
+            }
+            //--------------------------------------
+            //  Save Important Data in session
+            //--------------------------------------
+            $sessionStorage.token = config_app.token;
+            $sessionStorage.user = data.user;
+            $sessionStorage.userName = data.userName;
+            //$sessionStorage.AppId =
+
+            //---------------------------------------------
+            //--           Close Loading
+            //---------------------------------------------
+            $ionicLoading.hide();
+            $scope.$broadcast('scroll.refreshComplete');
+
+          } else if ("PAD" === pinCodeStatus ) {
+            $ionicLoading.hide();
+            $scope.$broadcast('scroll.refreshComplete');
+            PelApi.showPopup(config_app.pinCodeSubTitlePDA , "");
+          }else if("PCR" === pinCodeStatus){
+            $ionicLoading.hide();
+            $scope.$broadcast('scroll.refreshComplete');
+            errorMsg = config_app.PIN_STATUS.PAD;
+            PelApi.showPopup(config_app.pinCodeSubTitlePCR , "");
+          }else if("PWA" === pinCodeStatus){
+            $ionicLoading.hide();
+            $scope.$broadcast('scroll.refreshComplete');
+            errorMsg = config_app.PIN_STATUS.PAD;
+            PelApi.showPopup(config_app.pinCodeSubTitlePWA , "");
+          }else if("OLD" === pinCodeStatus){
+            $ionicLoading.hide();
+            $scope.$broadcast('scroll.refreshComplete');
+            errorMsg = config_app.PIN_STATUS.PAD;
+            PelApi.showPopupVersionUpdate(data.StatusDesc , "");
+          }
+        });
+      }
+      //--- ERROR ---//
+      , function () {
+        reMenu.success(function (data, status, headers, config) {
+          PelApi.writeToLog(config_app.LOG_FILE_ERROR_TYPE , "success : " + JSON.stringify(data));
+          $ionicLoading.hide();
+          $scope.$broadcast('scroll.refreshComplete');
+          PelApi.showPopup(config_app.getUserModuleTypesErrorMag , "");
+        }).error(function (data, status, headers, config) {
+          PelApi.writeToLog(config_app.LOG_FILE_ERROR_TYPE , "error : " + JSON.stringify(data));
+          $ionicLoading.hide();
+          $scope.$broadcast('scroll.refreshComplete');
+          PelApi.showPopup(config_app.getUserModuleTypesErrorMag , "");
+        });
+      }
+    );
+    }
+  }
+  //------------------------------------------------------
+  //--                  Switch APP
+  //------------------------------------------------------
+  $scope.appSwitch  = function(i) {
+
+      var iabOptions = {
+        location: 'yes',
+        clearcache: 'yes',
+        toolbar: 'yes'
+      };
+
+      var target = i.target || "_blank";
+      if(i.url) {
+        $cordovaInAppBrowser.open(i.url, target,iabOptions)
+          .then(function(event) {
+            // success
+          }, function(event) {
+            // error
+          });
+        return false;
+      } else if(i.Path) {
+        var path = i.Path; //"apps/" + i.Path + "/app.html";
+        window.location = path;
+      }
+
+    };
+  //-----------------------------------------------------------//
+  //--                 forwardToApp
+  //-----------------------------------------------------------//
+  $scope.forwardToApp = function (statePath , appId , titleDisp) {
+      if("wifi" === config_app.network){
+        $ionicLoading.hide();
+        $scope.$broadcast('scroll.refreshComplete');
+        PelApi.showPopup(config_app.wifiTitle , config_app.wifiSubTitle);
+      }else {
+
+        //$state.go(statePath, {AppId: appId, title: titleDisp, "pin": "0"});
+        //$sessionStorage.AppId2 = appId;
+        $sessionStorage.PeleAppId  = appId;
+        $sessionStorage.titleDisp = titleDisp;
+
+        var i = {};
+        // i.Path = statePath;
+        i.Path = "apps/DocApprove/app.html"; // Replace after modefication in SSO
+        $scope.appSwitch(i);
+
+      }
+    };
+  //-------------------------------//
+  //--       Code Section        --//
+  //-------------------------------//
+  var btnClass={};
+  btnClass.activ = false;
+  $scope.class = "pele-menu-item-on-touch item-icon-right";
+  $scope.doRefresh();
+})
+//======================================================================
+//==                          Settings
+//======================================================================
+.controller('SettingsListCtrl', [ '$scope'
+                                , '$fileLogger'
+                                , '$timeout'
+                                , '$state'
+                                , 'PelApi'
+                                , function($scope
+                                         , $fileLogger
+                                         , $timeout
+                                         , $state
+                                         , PelApi
+                                         ){
+
+    $scope.sendMail = function(){
+
+      $fileLogger.setStorageFilename(config_app.LOG_FILE_NAME);
+
+      $fileLogger.info("==================== END ====================");
+
+      $timeout(function(){
+
+        $fileLogger.checkFile().then(function(d) {
+
+          resolveLocalFileSystemURL(d.localURL.toString(), function(entry) {
+
+            cordova.plugins.email.open({
+              to:      'Mobile_Admins_HR@pelephone.co.il',
+              subject: config_app.LOG_FILE_MAIL_SUBJECT,
+              body:    '',
+              attachments:  entry.toURL()
+            });
+
+            PelApi.writeToLog(config_app.LOG_FILE_INFO_TYPE ,'=============== Send Email ==============');
+
+          }); // resolveLocalFileSystemURL
+        });
+      }, 8000);
+
+    }; // sendMail
+    //----------------------------------------------
+    //--             forwardTo
+    //----------------------------------------------
+    $scope.forwardTo = function(statePath){
+        $state.go(statePath);
+    };
+    //----------------------------------------------
+    //--          Update Version
+    //----------------------------------------------
+    $scope.updateAppVersion = function(){
+      var deviceInformation = ionic.Platform.device();
+      var isWebView = ionic.Platform.isWebView();
+      var isIPad = ionic.Platform.isIPad();
+      var isIOS = ionic.Platform.isIOS();
+      var isAndroid = ionic.Platform.isAndroid();
+      var isWindowsPhone = ionic.Platform.isWindowsPhone();
+
+      if(isAndroid){
+        window.open(config_app.GOOGLE_PLAY_APP_LINK, '_system', 'location=yes');
+      }
+      else if(isIOS){
+        window.open(config_app.APPLE_STORE_APP_LING, '_system', 'location=yes');
+      }
+    }// updateAppVersion
+
+  }])
+//========================================================================
+//--                       AppProfileCtrl
+//========================================================================
+.controller('AppProfileCtrl', [ '$scope'
+                                , '$fileLogger'
+                                , '$timeout'
+                                , 'PelApi'
+                                , function( $scope
+                                          , $fileLogger
+                                          , $timeout
+                                          , PelApi
+                                ) {
+
+      $scope.APP_VERSION = config_app.APP_VERSION;
+    }])
+
+;
