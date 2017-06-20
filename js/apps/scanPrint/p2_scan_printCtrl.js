4554a02d68b00f2266cb273d9d50d7bc0457aa51
OC2 <oren1@Mac-1.local>
2017-06-20 17:04:23 +0300

    popup

--- js/apps/scanPrint/p2_scan_printCtrl.js
+++ js/apps/scanPrint/p2_scan_printCtrl.js
@@ -2,7 +2,25 @@
  * Created by User on 12/12/2016.
  */
 var app = angular.module('pele.p2_scan_printCtrl', ['ngStorage']);
-app.controller('p2_scan_printCtrl', function($scope, $stateParams , $cordovaBarcodeScanner, $ionicLoading , PelApi){
+app.controller('p2_scan_printCtrl', function($scope, $stateParams , $cordovaBarcodeScanner, $ionicLoading ,$ionicPopup, PelApi){
+
+
+$scope.doSomething=function(){
+
+  var confirmPopup = $ionicPopup.confirm({
+       title: 'Consume Ice Cream',
+       template: 'Are you sure you want to eat this ice cream?'
+     });
+
+     confirmPopup.then(function(res) {
+       if(res) {
+         console.log('You are sure');
+       } else {
+         console.log('You are not sure');
+       }
+     });
+   };
+
     //-----------------------------------------//
     //--         scanBarcode
     //-----------------------------------------//
